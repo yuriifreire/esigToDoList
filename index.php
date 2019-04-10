e667bccc168b380bc0e26c37d7f5cb75d31fd917
Yuri Freire <yuriifreire@hotmail.com>
2019-04-10 11:27:05 -0300

    Add delete function and fix number order

--- index.php
+++ index.php
@@ -1,21 +1,32 @@
 <?php
+    $errors = "";
+
     //conectar ao banco
-    $db = mysqli_connect('localhost', 'root', '', 'todo');
+    $db = mysqli_connect('localhost', 'root', 'gela9914', 'todo');
 
     if (isset($_POST['submit'])) {
         $task = $_POST['task'];
+        if (empty($task)) {
+            $errors = "Não pode criar tarefa em branco!";
+        } else {
+            mysqli_query($db, "INSERT INTO tasks (task) VALUES ('$task')");
+            header('location: index.php');
+        }
+    }
 
-        mysqli_query($db, "INSERT INTO tasks (task) VALUES ('$task')");
+    // delete task
+    if (isset($_GET['del_task'])) {
+        $id = $_GET['del_task'];
+        mysqli_query($db, "DELETE FROM tasks WHERE id=$id");
         header('location: index.php');
     }
+
+    $tasks = mysqli_query($db, "SELECT * FROM tasks");
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ToDo List - ESIG</title>
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
@@ -25,6 +36,10 @@
     </div>
 
     <form method="POST" action="index.php">
+    <?php if (isset($errors)) { ?>
+        <p><?php echo $errors; ?></p>
+    <?php } ?>
+    
         <input type="text" name="task" class="task_input">
         <button type="submit" class="task_button" name="submit">Adicionar Tarefa</button>
     </form>
@@ -39,14 +54,16 @@
         </thead>
 
         <tbody>
+        <?php $i = 1; while ($row = mysqli_fetch_array($tasks)) { ?>
             <tr>
-                <td>1</td>
-                <td class="task">Primeira tarefa</td>
+                <td><?php echo $i; ?></td>
+                <td class="task"><?php echo $row['$task']; ?></td>
                 <td></td>   <!-- Não sei o porque, mas sem colocar esse td, o X não fica alinhado  -->
                 <td class="delete">
-                    <a href="#">X</a>
+                    <a href="index.php?del_task=<?php echo $row['id']; ?>">X</a>
                 </td>
             </tr>
+        <?php $i++; } ?>
         </tbody>
     </table>
 </body>
